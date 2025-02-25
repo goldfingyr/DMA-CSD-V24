5358853689fabb66782bda2b2591387f2c34c68d
Karsten Jeppesen <kaje@ucn.dk>
2025-02-25 11:21:55 +0100

    Telegram + ACID-1

---
+++ Telegram/Telegram/Program.cs
@@ -0,0 +1,16 @@
+﻿void SendMessage(string message)
+{
+    var client = new HttpClient();
+    var request = new HttpRequestMessage(HttpMethod.Post, "https://api.telegram.org/bot5949217810:AAFqqP1QyJAtrp4_jermdlLGBXiCzAqoJk8/sendMessage");
+    var collection = new List<KeyValuePair<string, string>>();
+    collection.Add(new("chat_id", "-1002370042141"));
+    collection.Add(new("text", message));
+    var content = new FormUrlEncodedContent(collection);
+    request.Content = content;
+    var response = client.SendAsync(request).Result;
+    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+}
+
+
+
+SendMessage("Hello, World!");
